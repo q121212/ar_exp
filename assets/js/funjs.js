a004fa0d8e34d9d3b74dd8a03758e81e7bcb57e0
MaxR <q121212@gmail.com>
2018-06-20 02:50:00 +0300

    Was added modal/popup for paramChanger functionality

--- assets/js/funjs.js
+++ assets/js/funjs.js
@@ -23,12 +23,37 @@ window.onload = function(){
     var output = document.getElementById('output');
     inpTextArea.addEventListener('input', function (){
         output.innerHTML= inpTextArea.value;
-        console.log(output);
+        // console.log(output);
     });
 
+    var paramsChanger = document.getElementById('paramsChanger');
     output.addEventListener('click', function () {
-       // TODO: Parser for vars in text
-        alert(1);
+        paramsChanger.style.display == 'block' ? paramsChanger.style.display = 'none' : paramsChanger.style.display = 'block';
+        var closeBtn = document.getElementById('closeBtn');
+        var cancelBtn = document.getElementById('cancelBtn');
+        closeBtn.addEventListener('click', function () {
+            paramsChanger.style.display = 'none';
+
+        });
+        cancelBtn.addEventListener('click', function () {
+            paramsChanger.style.display = 'none';
+        });
+
+        var applyBtn = document.getElementById('applyBtn');
+        applyBtn.addEventListener('click', function () {
+            paramsChanger.style.display = 'none';
+
+
+            // TODO: Parser for vars in text
+            // function numsHandler(){
+            //     console.log(output.value.split(/\r?\n|\s/g));
+            // }
+            // numsHandler();
+
+
+            output.innerHTML='Updated text';
+
+        });
 
     });
 
